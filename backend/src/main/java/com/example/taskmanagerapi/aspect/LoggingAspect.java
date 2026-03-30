789b3a8be2d385eb6313085afe413058d7e58946
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-03-30 12:24:04 +0200

    update API port to 8082 and adjust frontend config

--- backend/src/main/java/com/example/taskmanagerapi/aspect/LoggingAspect.java
+++ backend/src/main/java/com/example/taskmanagerapi/aspect/LoggingAspect.java
@@ -5,13 +5,19 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+@Aspect // Marks this class as an Aspect (AOP component)
+@Component // Registers this class as a Spring Bean
 public class LoggingAspect {
 
+    // This advice runs BEFORE execution of any method
+    // in the service package
     @Before("execution(* com.example.taskmanagerapi.service.*.*(..))")
     public void logMethodCall(JoinPoint joinPoint) {
+
+        // Get the name of the called method
         String methodName = joinPoint.getSignature().getName();
+
+        // Log method execution to console
         System.out.println("👉 Method called: " + methodName);
     }
 }
--- backend/src/main/java/com/example/taskmanagerapi/controller/TaskController.java
+++ backend/src/main/java/com/example/taskmanagerapi/controller/TaskController.java
@@ -20,7 +20,7 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/api/tasks")
 @RequiredArgsConstructor
 public class TaskController {
 
